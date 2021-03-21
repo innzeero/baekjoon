8b0e292c053a5a11902de7daecd39f4ee4478e33
innzeero <innzeero@gmail.com>
2021-03-21 21:46:29 +0900

    calculations.py

---
+++ baekjoon/010_calculations.py
@@ -0,0 +1,22 @@
+# --url--
+# https://www.acmicpc.net/problem/10869
+
+# --title--
+# 10869번: 사칙연산
+
+# --problem_description--
+# 두 자연수 A와 B가 주어진다. 이때, A+B, A-B, A*B, A/B(몫), A%B(나머지)를 출력하는 프로그램을 작성하시오. 
+
+# --problem_input--
+# 두 자연수 A와 B가 주어진다. (1 ≤ A, B ≤ 10,000)
+
+# --problem_output--
+# 첫째 줄에 A+B, 둘째 줄에 A-B, 셋째 줄에 A*B, 넷째 줄에 A/B, 다섯째 줄에 A%B를 출력한다.
+
+a, b = map(int, input().split())
+
+print(a+b)
+print(a-b)
+print(a*b)
+print(a//b)
+print(a % b)
