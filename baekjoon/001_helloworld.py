e6bbf64226e8bcd97835eadf8da4ceefc97e7ad3
innzeero <innzeero@gmail.com>
2021-03-21 21:07:18 +0900

    day1

---
+++ baekjoon/001_helloworld.py
@@ -0,0 +1,26 @@
+# --url--
+# https://www.acmicpc.net/problem/2557
+
+# --title--
+# 2557번: Hello World
+
+# --problem_description--
+
+
+# 	Hello World!를 출력하시오.
+
+# --problem_input--
+
+
+# 	없음
+
+# --problem_output--
+
+
+# 	Hello World!를 출력하시오.
+
+# trial and error
+# python에서는 print 함수를 사용하여 내용을 출력할 수 있다.
+# print() 안에 "" 또는 ''와 함께 입력하면 된다.
+
+print("Hello World!")
