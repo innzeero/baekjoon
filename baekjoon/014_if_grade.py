3e19d886277f43228e5b65f45b809fbc3462cb8c
innzeero <innzeero@gmail.com>
2021-03-24 21:24:02 +0900

    if

---
+++ baekjoon/014_if_grade.py
@@ -0,0 +1,27 @@
+# --url--
+# https://www.acmicpc.net/problem/9498
+
+# --title--
+# 9498번: 시험 성적
+
+# --problem_description--
+# 시험 점수를 입력받아 90 ~ 100점은 A, 80 ~ 89점은 B, 70 ~ 79점은 C, 60 ~ 69점은 D, 나머지 점수는 F를 출력하는 프로그램을 작성하시오.
+
+# --problem_input--
+# 첫째 줄에 시험 점수가 주어진다. 시험 점수는 0보다 크거나 같고, 100보다 작거나 같은 정수이다.
+
+# --problem_output--
+# 시험 성적을 출력한다.
+
+score = int(input())
+
+if 90 <= score <= 100:
+    print("A")
+elif 80 <= score < 90:
+    print("B")
+elif 70 <= score < 80:
+    print("C")
+elif 60 <= score < 70:
+    print("D")
+else:
+    print("F")
