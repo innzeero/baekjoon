377a466c17c8e0e27ebaa1b15bf3c784e76cc588
innzeero <innzeero@gmail.com>
2021-03-27 17:19:11 +0900

    git_commit

--- baekjoon/016_alarm.py
+++ baekjoon/016_alarm.py
@@ -25,15 +25,22 @@
 # --problem_output--
 # 첫째 줄에 상근이가 창영이의 방법을 사용할 때, 설정해야 하는 알람 시간을 출력한다. (입력과 같은 형태로 출력하면 된다.)
 
-h1, m1 = map(int, input().split())
+h, m = map(int, input().split())
 
 # print(h, m)
 
-# m2 = (m1 + 60) - 45
+# my code
+# if m >= 45:
+#     print(h, m-45)
+# elif m < 45 and h == 0:
+#     print(23, m+15)
+# elif m < 45 and h != 0:
+#     print(h-1, m+15)
 
-if m1 >= 45:
-    print(h1, m1-45)
-elif m1 < 45 and h1 == 0:
-    print(23, m1+15)
-elif m1 < 45 and h1 != 0:
-    print(h1-1, m1+15)
+# clean ver
+if m >= 44:
+    print(h, m-45)
+elif m < 45 and h >= 1:
+    print(h-1, m+15)
+else:
+    print(23, m+15)
